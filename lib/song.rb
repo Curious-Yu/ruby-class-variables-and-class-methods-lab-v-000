a8318c5967040c9b7089e762f819389cb32d658c
Wen Yu <wyu10amy@hotmail.com>
2021-06-07 20:50:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,19 @@
 class Song
 
   @@count = 0
+  @@artisits = 0
+  @@genre = 0
 
   attr_accessor :name, :artist, :genre
 
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genere
+    @count = count + 1
+  end
 
-
+  def count
   end
 
 end
